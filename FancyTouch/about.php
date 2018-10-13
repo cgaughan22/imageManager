8e4fd494f840e4293133fe13f15bcdb42d28a696
cgaughan22 <cgaughan22@gmail.com>
2018-10-13 10:48:53 -0700

    about.php last one

---
+++ FancyTouch/about.php
@@ -0,0 +1,27 @@
+<html lang="en">
+	<head>
+		<title>About: Fancy Touch</title>
+		<?php include 'globalHead.php';?>
+	</head>
+
+	<body>
+		<?php include 'nav-welcome.php';?>
+		<div id="content">
+			<div class="content">
+				<h3>ABOUT US
+			</div>
+			<div class="contentText">
+
+					Hi there! Welcome to my page.  I’m Gisselle, the photographer behind the lens.  I studied Communications and Broadcast Journalism at Berea College for four years.  There, i gained the necessary knowledge to bring images to life…. Since I can remember I have always loved taking photos.  There is something about looking back in time that brings so many emotions.  Now looking back in time to images that come to life is something else!!  For this reason, I have decided to continue my passion for photography and share my perspective by creating wonderful memor	ies for amazing people like you!</p>
+
+				While in college i was confident my happiness laid  working in a news-station, Univision.  For the rest of my life I wanted to make a difference in people's lives by sharing the education i earned through an unbiased perspective and hard work.  I also worked for Women in Film and Video where i was able to get my hands in the film world.  Image Makers taught me the value of being respected as a humbled leader that cares to understand others.  Today, i am ready to start something big of my own.  And, i would love for you to join me! <hr>
+				&hearts; &hearts; &hearts; &hearts;<br>
+				Having a family of my own, i know how precious it is to capture especial occasions, especially if you have children.  They just grow too fast! Also, having a child of my own, i know first hand how difficult it can be to capture that perfect shot that when everyone takes a glance at, it makes them say aww.  I am super experienced with this.  My child blessed me with the challenge that i still take on every few months.  I consider myself to be a passionate lover.  Sometimes i feel i was born in the wrong era. This reflects in the way i capture fairy tale like images.  And, owning a Nikon D71000 allows me to get it right every time! 
+
+				Special occasions such as wedding are amazing days that take a lot of hard work.  I know this from experience.  I take pride in being organized, reliable and flexible with your arrangements (even if they change by the minute :)) When you shoot with me, my goal is to make you feel as comfortable as possible so that you can be yourself and enjoy the fun the lens brings. <hr> 
+				&hearts; &hearts; &hearts; &hearts;
+			</div>
+		</div>
+		<?php include 'footer.php';?> 
+	</body>
+</html>
